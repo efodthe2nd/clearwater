a0aa00809a075769c98e7f8f37d15a573b9123a0
David Efod <davidefod@gmail.com>
2026-02-24 11:59:29 +0100

    icon fix

--- app/components/sell/PaymentProcess.tsx
+++ app/components/sell/PaymentProcess.tsx
@@ -25,7 +25,7 @@ const payments = [
   {
     icon: (
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
-        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" fill="#F7F217" opacity="0.2" stroke="#F7F217" strokeWidth="1.5"/>
+        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" fill="#F7F217" stroke="#F7F217" strokeWidth="1.5"/>
       </svg>
     ),
     title: "You're in Control",
